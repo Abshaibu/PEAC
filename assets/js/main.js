c867149b294d8e4f982fcb747301056ba3f2d314
abshaibu <shaibuabdulsalam56@gmail.com>
2022-04-14 18:49:26 +0100

    finished mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
 const toggleBtn = document.querySelector('.toggle-btn');
 const navLinks = document.querySelector('.nav-links');
-const closeBtn = document.querySelector('.close-btn');
+const closeBtn = document.querySelector('.cl-btn');
 const links = document.querySelectorAll('.nav-link');
 
 // menu toggler
-// toggleBtn.addEventListener('click', (e) => {
-//     navLinks.classList.add('show-navlinks');
-// });
+toggleBtn.addEventListener('click', (e) => {
+    navLinks.classList.add('show-navlinks');
+    document.querySelector('.nav-list-container').style.display = 'block';
+});
 
-// closeBtn.addEventListener('click', (e) => {
-//     navLinks.classList.remove('show-navlinks');
-// });
+closeBtn.addEventListener('click', (e) => {
+    navLinks.classList.remove('show-navlinks');
+    document.querySelector('.nav-list-container').style.display = 'none';
+});
 
-//  scroll to specific section
-// links.forEach((item) => {
-//     item.addEventListener('click', () => {
-//         navLinks.classList.remove('show-navlinks');
-
-//     });
-// });
+// scroll to specific section
+links.forEach((item) => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('show-navlinks');
+    });
+});
 
 
 // date
