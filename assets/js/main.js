caea9b861eb712e4262a0baa09363d08ca516bf4
abshaibu <shaibuabdulsalam56@gmail.com>
2022-04-16 13:35:58 +0100

    added preview features v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,20 @@ const toggleBtn = document.querySelector('.toggle-btn');
 const navLinks = document.querySelector('.nav-links');
 const closeBtn = document.querySelector('.cl-btn');
 const productBtn = document.querySelectorAll('.product-btn');
+const prdTwo = document.querySelectorAll('.prd-two');
 const orderMessage = document.querySelector('.order-message');
 const messageBtn = document.querySelector('.message-btn');
 const links = document.querySelectorAll('.nav-link');
+
+// products tabs
+$(document).ready(function () {
+    $('.tab-a').click(function () {
+        $(".tab").removeClass('tab-active');
+        $(".tab[data-id='" + $(this).attr('data-id') + "']").addClass("tab-active");
+        $(".tab-a").removeClass('active-a');
+        $(this).parent().find(".tab-a").addClass('active-a');
+    });
+});
 
 // menu toggler
 toggleBtn.addEventListener('click', (e) => {
@@ -39,6 +50,12 @@ orderMessage.addEventListener('click', () => {
     orderMessage.classList.remove('show-order');
 });
 
+// products overview
+prdTwo.forEach((item) => {
+    item.addEventListener('click', () => {
+        orderMessage.classList.add('show-order');
+    });
+});
 
 // date
 const date = document.querySelector('.date');
